a35404eee7e4723da2f57a7e4304577ccfb16195
watagassa <k23095kk@aitech.ac.jp>
2024-04-04 11:45:42 +0900

    latlon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
+import { LatLonToLoc } from "./components/LatLonToLoc";
 import { Kahun } from "./components/Kahun";
 import { WitchCode } from "./components/WhichCode";
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
+        <LatLonToLoc></LatLonToLoc>
         <Kahun></Kahun>
         <WitchCode></WitchCode>
       </header>
